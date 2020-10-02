a26f6a611b3f871dbdf5f11750d7ada7a0cd0dd3
Sojisoyoye <soji.soyoye@gmail.com>
2020-10-02 17:30:56 +0100

    app setup - create controllers, routes, models folders - create validators, middlewares - installed necessary dependencies

---
+++ app.js
@@ -0,0 +1,40 @@
+// import express from 'express';
+// import dotenv from 'dotenv';
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+
+
+const app = express();
+
+// dotenv.config()
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user')
+
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+})
+.then(() => console.log("DB Connected"));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const { userById } = require('../controllers/user');
+
+router.param('userId', userById);
+
+module.exports = router;
